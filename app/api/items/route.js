4d5fb4fac166d3aad30f6ac6c4d2a66a57972fec
Benly <benly.c@lezdotechmed.com>
2024-11-09 09:31:42 +0530

    route handlers

--- app/api/items/route.js
+++ app/api/items/route.js
@@ -1,3 +1,7 @@
 export async function GET() {
   return Response.json([{ name: "xx" }, { name: "yy" }, { name: "zz" }]);
 }
+
+export async function POST() {
+  return Response.json({ message: "Post successful" });
+}
